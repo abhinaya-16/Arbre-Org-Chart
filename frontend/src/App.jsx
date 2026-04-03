13518a29378d17b34adb9786959d8b9a4a11a7b7
abhinayakondi <abhisonukondi123@gmail.com>
2026-04-03 18:22:39 -0400

    Sign In Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/Sign-in/SignIn";
 import OrgChartPage from "./pages/OrgChartPage";
 
 function App() {
-  return <OrgChartPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/org-chart" element={<OrgChartPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+// function App() {
+//   return (
+//     <OrgChartPage />
+//   );
+// }
 
 export default App;
